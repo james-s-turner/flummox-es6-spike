668403c534bcc4df37e8f3a0a07238ea60910a3e
james-s-turner <james.shaw.turner@gmail.com>
2015-06-08 09:43:18 +0100

    get es6 working by using traceur and .js suffix

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var System = require('es6-module-loader').System;
-System.transpiler = 'babel';
-
+System.transpiler = 'traceur';
 
 console.log("started");
 // loads './app.js' from the current directory
-System.import('./app').then(function(m) {
+System.import('./app.js').then(function(m) {
+    console.log("loading");
     console.log(m);
 });
+
+console.log("finished");
